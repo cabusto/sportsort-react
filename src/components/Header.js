54ca9a2dc88fe2491b1f71ccfde0a32b98cd677e
Justin Warren <justinkwarren@gmail.com>
2019-11-05 10:24:48 -0330

    Convert Header to functional component
    Alternate ES6 way of writing a stateless functional component
    
    - The rocket notation could also be writtin as const Header = (props) => 
    and then use props.propname later in function
    - Maybe a performance boost if the component is stateless

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,20 @@
 import * as React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Player
-          <span className="ofThe">
-            <span className="of">Of</span>
-            <span className="the">The</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline">
-          <span>{this.props.tagline}</span>
-        </h3>
-      </header>
-    );
-  }
-}
+const Header = ({ tagline, days }) => (
+  <header className="top">
+    <h1>
+      Player
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{tagline}</span>
+      <span>{days}</span>
+    </h3>
+  </header>
+);
 
 export default Header;
