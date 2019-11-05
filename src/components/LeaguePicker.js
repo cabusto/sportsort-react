88ac8549667e998cf3dd787b8fa3f80a1e33451e
Justin Warren <justinkwarren@gmail.com>
2019-11-05 11:29:20 -0330

    Binding 'this' in custom methods
    Accessing this in a custom method, use the arrow notation to create a property

--- src/components/LeaguePicker.js
+++ src/components/LeaguePicker.js
@@ -1,13 +1,29 @@
 import * as React from "react";
 
 class LeaguePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // stop the form from submitting
+    event.preventDefault();
+    // get text from input (Golden rule of react: Don't touch the DOM! use state if can)
+    console.log(this);
+    //Change page to /store/whatever-they-entered
+  };
+
   render() {
     return (
       <React.Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           {/* curly means run a js code, like a commnet */}
           <h2>Search Players</h2>
-          <input type="text" required placeholder="Select League" />
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Select League"
+            defaultValue="blah"
+          />
           <button type="submit">Find League</button>
         </form>
       </React.Fragment>
